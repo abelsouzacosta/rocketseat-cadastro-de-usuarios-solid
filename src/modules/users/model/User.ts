ba0869f2d2229215181620e1807c19c2b19fb040
Abel Souza <abelsouzacosta@gmail.com>
2022-01-10 00:11:57 -0300

    adding model: User

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,18 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  protected id?: string;
+  protected name: string;
+  protected admin: boolean;
+  protected email: string;
+  protected created_at: Date;
+  protected updated_at: Date;
+
+  constructor(name: string, email: string) {
+    this.id = this.id ? this.id : uuidV4();
+    this.name = name;
+    this.email = email;
+  }
 }
 
 export { User };
